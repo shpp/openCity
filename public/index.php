e9ebe7a537902603732648a738b991a158cdacd3
OlegPid <m0509115356@gmail.com>
2017-03-02 13:44:13 +0200

    Fix index.php

--- public/index.php
+++ public/index.php
@@ -34,6 +34,10 @@ require __DIR__.'/../bootstrap/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+// set the public path to this directory
+$app->bind('path.public', function() {
+    return __DIR__;
+});
 
 /*
 |--------------------------------------------------------------------------
