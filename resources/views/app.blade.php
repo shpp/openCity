2e6b08d1f77a256ffb1f154af6ca06c0379649a3
Karponter <Karponter.tarino@gmail.com>
2017-09-18 18:44:37 +0300

    indetation fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,138 +20,138 @@
     @include('components.analytics')
 </head>
 <body>
-    <aside>
-        <ul id="nav-mobile" class="side-nav fixed">
-            <li class="logo">
-                <a href="{{ url('/') }}" class="brand-logo green-text">Мicто для вcix</a>
-            </li>
-            <li class="no-padding">
-                <ul class="collapsible collapsible-accordion">
-                    <li class="bold active">
-                        <a class="collapsible-header waves-effect waves-green active">
-                            Категорії мiсць
-                        </a>
-                        <div class="collapsible-body">
-                            <form id="categories-form">
-                                <ul id="categories" style="max-height: 50vh; overflow: auto; position: relative;">
-                                    <li>
-                                        <input type="checkbox" id="checkAll" class="filled-in" checked/>
-                                        <label for="checkAll" class="black-text">Усі місця</label>
-                                    </li>
-                                    @foreach ($categories as $category)
+<aside>
+    <ul id="nav-mobile" class="side-nav fixed">
+        <li class="logo">
+            <a href="{{ url('/') }}" class="brand-logo green-text">Мicто для вcix</a>
+        </li>
+        <li class="no-padding">
+            <ul class="collapsible collapsible-accordion">
+                <li class="bold active">
+                    <a class="collapsible-header waves-effect waves-green active">
+                        Категорії мiсць
+                    </a>
+                    <div class="collapsible-body">
+                        <form id="categories-form">
+                            <ul id="categories" style="max-height: 50vh; overflow: auto; position: relative;">
+                                <li>
+                                    <input type="checkbox" id="checkAll" class="filled-in" checked/>
+                                    <label for="checkAll" class="black-text">Усі місця</label>
+                                </li>
+                                @foreach ($categories as $category)
                                     <li class="category-toggle">
                                         <input type="checkbox"
-                                        name="cat[]"
-                                        id="cat{{$category->id}}"
-                                        value="{{$category->id}}"
-                                        class="filled-in"
-                                        checked/>
+                                               name="cat[]"
+                                               id="cat{{$category->id}}"
+                                               value="{{$category->id}}"
+                                               class="filled-in"
+                                               checked/>
                                         <label for="cat{{$category->id}}" class="black-text">
                                             {{$category->name}}
                                         </label>
                                     </li>
-                                    @endforeach
-                                </ul>
-                            </form>
-                        </div>
-                    </li>
-                    <li class="bold">
-                        <a class="collapsible-header waves-effect waves-green">Параметри зручності</a>
-                        <div class="collapsible-body">
-                            <form id="access-form">
-                                <ul id="accessibility">
-                                    @foreach ($accessibilities as $accessibility)
+                                @endforeach
+                            </ul>
+                        </form>
+                    </div>
+                </li>
+                <li class="bold">
+                    <a class="collapsible-header waves-effect waves-green">Параметри зручності</a>
+                    <div class="collapsible-body">
+                        <form id="access-form">
+                            <ul id="accessibility">
+                                @foreach ($accessibilities as $accessibility)
                                     <li>
                                         <input type="checkbox"
-                                        name="acc[]"
-                                        id="acc{{$accessibility->id}}"
-                                        value="{{$accessibility->id}}"
-                                        class="filled-in"/>
+                                               name="acc[]"
+                                               id="acc{{$accessibility->id}}"
+                                               value="{{$accessibility->id}}"
+                                               class="filled-in"/>
                                         <label for="acc{{$accessibility->id}}" class="black-text">
                                             {{$accessibility->name}}
                                         </label>
                                     </li>
-                                    @endforeach
-                                </ul>
-                            </form>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="https://play.google.com/store/apps/details?id=me.kowo.opencity"
+                                @endforeach
+                            </ul>
+                        </form>
+                    </div>
+                </li>
+                <li>
+                    <a href="https://play.google.com/store/apps/details?id=me.kowo.opencity"
                         class="btn green waves waves-light"
                         target="_blank">
                             <i class="material-icons white-text">android</i>
                             Завантажити</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </aside>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
 
-    <main>
-        <div class="content">
-            <div class="search-wrapper card">
-                <input id="search-input" placeholder="Почніть набирати назву закладу..." class="typeahead">
-                <i class="material-icons">search</i>
-            </div>
-            <div class="login-float-block">
-                @if (auth()->guest())
+<main>
+    <div class="content">
+        <div class="search-wrapper card">
+            <input id="search-input" placeholder="Почніть набирати назву закладу..." class="typeahead">
+            <i class="material-icons">search</i>
+        </div>
+        <div class="login-float-block">
+            @if (auth()->guest())
                 <a class="login-float__link modal-trigger" href="#login_modal">Увійти</a>
                 @else
                 <div class="user-control-block">{{ auth()->user()->name }}</div>
-                @endif
-            </div>
-            <div id="map"></div>
-            @include('components.right-bar')
-            <a class="waves-effect waves-light btn modal-trigger main-trolley__button light-blue lighten-5 black-text" href="#low_trolley_modal">
-                <i class="material-icons left">directions_bus</i>
-                Низькополий тролейбус</a>
-            <a class="waves-effect waves-light btn modal-trigger main-social-taxi__button yellow accent-2 black-text" href="#social_taxi_modal">
-                <i class="material-icons left">local_taxi</i>
-                Соціальне таксі</a>
+            @endif
         </div>
-    </main>
+        <div id="map"></div>
+        @include('components.right-bar')
+        <a class="waves-effect waves-light btn modal-trigger main-trolley__button light-blue lighten-5 black-text" href="#low_trolley_modal">
+            <i class="material-icons left">directions_bus</i>
+            Низькополий тролейбус</a>
+        <a class="waves-effect waves-light btn modal-trigger main-social-taxi__button yellow accent-2 black-text" href="#social_taxi_modal">
+            <i class="material-icons left">local_taxi</i>
+            Соціальне таксі</a>
+    </div>
+</main>
 
-    <footer class="page-footer green">
-        <div class="custom-footer-container">
-            <div class="footer-description">
-                <h5 class="white-text">Соціальний проект «Місто для всіх»</h5>
-                <p class="white-text text-lighten-4">
-                    Це карта доступності міста Кропивницький для людей з особливими потребами та батьків з маленькими
-                    дітьми, за допомогою якої можна визначити об’єкт на карті та дізнатися його координати, контакти та
-                    атрибути доступності (наявність пандуса та кнопки виклику).
-                    Ми працюємо над тим, щоб наповнити базу проекту і завди раді допомозі. Тож якщо ви бажаєте долучитися та
-                    зробити наше місто зручнішим для всіх &mdash; зв'яжіться з нами:
-                </p>
-                <ul class="footer-contacts white-text">
-                    <li>
-                        <i class="material-icons">phone</i>
-                        <a href="tel:+380952409572">+380952409572</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="footer-form">
-                <h5 class="white-text footer-form-heading">Напишіть нам!</h5>
-                <form id="message-form">
-                    {{ csrf_field() }}
-                    <!-- TODO -->
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="email" type="email" class="validate" name="email">
-                            <label for="email">Email</label>
-                        </div>
+<footer class="page-footer green">
+    <div class="custom-footer-container">
+        <div class="footer-description">
+            <h5 class="white-text">Соціальний проект «Місто для всіх»</h5>
+            <p class="white-text text-lighten-4">
+                Це карта доступності міста Кропивницький для людей з особливими потребами та батьків з маленькими
+                дітьми, за допомогою якої можна визначити об’єкт на карті та дізнатися його координати, контакти та
+                атрибути доступності (наявність пандуса та кнопки виклику).
+                Ми працюємо над тим, щоб наповнити базу проекту і завди раді допомозі. Тож якщо ви бажаєте долучитися та
+                зробити наше місто зручнішим для всіх &mdash; зв'яжіться з нами:
+            </p>
+            <ul class="footer-contacts white-text">
+                <li>
+                    <i class="material-icons">phone</i>
+                    <a href="tel:+380952409572">+380952409572</a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-form">
+            <h5 class="white-text footer-form-heading">Напишіть нам!</h5>
+            <form id="message-form">
+            {{ csrf_field() }}
+            <!-- TODO -->
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="email" type="email" class="validate" name="email">
+                        <label for="email">Email</label>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <textarea name="text" id="message" class="materialize-textarea"></textarea>
-                            <label for="message">Ваш комментар</label>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea name="text" id="message" class="materialize-textarea"></textarea>
+                        <label for="message">Ваш комментар</label>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <button class="btn white green-text waves-effect waves-light"
-                            type="submit"
-                            name="action">
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <button class="btn white green-text waves-effect waves-light"
+                                type="submit"
+                                name="action">
                             Вiдправити
                             <i class="material-icons right">send</i>
                         </button>
@@ -165,29 +165,29 @@
             <div class="col s6">
                 © 2017 made with love by
                 <a href="http://programming.kr.ua"
-                class="white-text"
-                target="_blank">
-                Ш++
-            </a>
-        </div>
-        <div class="col s6 right-align">
-            <a href="https://www.facebook.com/groups/1531774503500273/?fref=ts" target="_blank">
-                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 167.657 167.657"
-                style="enable-background:new 0 0 167.657 167.657; width: 25px; height: 25px"
-                xml:space="preserve">
-                <g>
-                    <path style="fill:#fff;" d="M83.829,0.349C37.532,0.349,0,37.881,0,84.178c0,41.523,30.222,75.911,69.848,82.57v-65.081H49.626
-                    v-23.42h20.222V60.978c0-20.037,12.238-30.956,30.115-30.956c8.562,0,15.92,0.638,18.056,0.919v20.944l-12.399,0.006
-                    c-9.72,0-11.594,4.618-11.594,11.397v14.947h23.193l-3.025,23.42H94.026v65.653c41.476-5.048,73.631-40.312,73.631-83.154
-                    C167.657,37.881,130.125,0.349,83.829,0.349z"/>
-                </g></svg>
-                <span class="foot-facebook-link__text"> Місто для всіх</span>
-            </a>
+                   class="white-text"
+                   target="_blank">
+                    Ш++
+                </a>
+            </div>
+            <div class="col s6 right-align">
+                <a href="https://www.facebook.com/groups/1531774503500273/?fref=ts" target="_blank">
+                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         viewBox="0 0 167.657 167.657"
+                         style="enable-background:new 0 0 167.657 167.657; width: 25px; height: 25px"
+                         xml:space="preserve">
+<g>
+    <path style="fill:#fff;" d="M83.829,0.349C37.532,0.349,0,37.881,0,84.178c0,41.523,30.222,75.911,69.848,82.57v-65.081H49.626
+        v-23.42h20.222V60.978c0-20.037,12.238-30.956,30.115-30.956c8.562,0,15.92,0.638,18.056,0.919v20.944l-12.399,0.006
+        c-9.72,0-11.594,4.618-11.594,11.397v14.947h23.193l-3.025,23.42H94.026v65.653c41.476-5.048,73.631-40.312,73.631-83.154
+        C167.657,37.881,130.125,0.349,83.829,0.349z"/>
+</g></svg>
+                    <span class="foot-facebook-link__text"> Місто для всіх</span>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 </footer>
 <!-- Modal Structure -->
 <div id="low_trolley_modal" class="modal">
@@ -281,7 +281,7 @@ async defer></script>
   $(document).ready(function(){
     MassToggler('aside input#checkAll', 'aside li.category-toggle input'); 
     $('.modal').modal();
-});
+  });
 </script>
 </body>
 </html>
